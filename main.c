263e67c1fa7956a73dbee14a39295c09e396171e
slimeenergy <48282719+slimeenergyreal@users.noreply.github.com>
2021-10-03 17:36:20 -0400

    Update main.c

--- main.c
+++ main.c
@@ -6,19 +6,35 @@
  * Description: test program
  *--------------------------------------
 */
+
+#define JANUS_ANIMATION
+
 #include <tice.h>
 #include <graphx.h>
 #include <keypadc.h>
 
 #include "janus/janus.h"
+#include "gfx/gfx.h"
+
+#define SCENE_MAIN_DEMO 0
+#define SCENE_EASING_DEMO 1
+#define SCENE_ANIMATION_DEMO 2
+#define SCENE_DEBOUNCING_DEMO 3
+
+#define PLAYER_SPEED 10
+
+#define PLAYER_STARTING_X LCD_WIDTH/2 + 50
+#define PLAYER_STARTING_Y LCD_HEIGHT/2
 
 struct Demo
 {
     uint24_t playerX;
+    uint8_t playerY;
     uint8_t scene;
     uint8_t lastSelected;
-    struct janus_Animation animation;
+    struct janus_Ease ease;
     struct janus_QuickUI ui;
+    struct janus_Animation animation;
 };
 
 void renderFps(float elapsedTime);
@@ -32,17 +48,27 @@ void render(struct Demo* demo, float elapsedTime);
 
 int main(void)
 {
+
     struct Demo demo = {
-        0,0,0,
-        /* Animation */
+        PLAYER_STARTING_X,PLAYER_STARTING_Y,0,0,
+        /* Ease */
         {QUAD_IN_OUT,true,50,170,250,20,50,50,2,0},
         /* QuickUI */
-        {ELASTIC,"Janus Library Demo",0,0,0xFF,0x00,0xEC,0,3,{"Play","Options","Credits"}}
+        {ELASTIC,"Janus Library Demo",0,0,0xFF,0x01,0xEC,0,4,{"All Demos","Easing Demo","Animation Demo","Debouncing Demo"}},
+        /* Animation */
+        {
+            12,{wizard_tile_0,wizard_tile_1,wizard_tile_2,wizard_tile_3,wizard_tile_4,wizard_tile_5,wizard_tile_6,wizard_tile_7,wizard_tile_8,wizard_tile_9,wizard_tile_10,wizard_tile_11},
+            true,{2.0f,0.1f,1.0f,0.1f,0.1f,0.1f,0.5f,0.1f,0.1f,0.1f,0.2f,0.2f},
+            0.0f,true,0.0f,0
+        }
     };
 
     srand(rtc_Time());
 
     gfx_Begin();
+
+    gfx_SetPalette(wizardpalette,sizeof_wizardpalette,0);
+    gfx_SetTransparentColor(0);
 
     while(!(kb_Data[6] & kb_Clear)) {
         float elapsedTime = janus_GetElapsedTime();
@@ -63,45 +89,83 @@ int main(void)
 
 void update(struct Demo* demo, float elapsedTime) {
     bool selected;
+
     /* Debouncing Demo */
-    if(janus_PressedKeys[7] & kb_Right) {
-        demo->playerX += 10;
-    }
-    
-    if(janus_PressedKeys[7] & kb_Left) {
-        demo->playerX -= 10;
-    }
-    /* Animation Easing Demo*/
+    if(demo->scene == SCENE_MAIN_DEMO || demo->scene == SCENE_DEBOUNCING_DEMO) {
 
-    if(demo->animation.progress == demo->animation.length || demo->animation.progress == 0) {
-        // Reverse animation when it reaches an end
-        demo->animation.reverse = !demo->animation.reverse;
+        if(janus_PressedKeys[7] & kb_Right) {
+            demo->playerX += PLAYER_SPEED;
+        }
+        if(janus_PressedKeys[7] & kb_Left) {
+            demo->playerX -= PLAYER_SPEED;
+        }
+        if(janus_PressedKeys[7] & kb_Up) {
+            demo->playerY -= PLAYER_SPEED;
+        }
+        if(janus_PressedKeys[7] & kb_Down) {
+            demo->playerY += PLAYER_SPEED;
+        }
+
     }
 
-    janus_UpdateAnimation(&demo->animation,elapsedTime);
+    /* Easing Demo */
+    if(demo->scene == SCENE_MAIN_DEMO || demo->scene == SCENE_EASING_DEMO) {
+
+        if(demo->ease.progress == demo->ease.length || demo->ease.progress == 0) {
+            // Reverse ease when it reaches an end
+            demo->ease.reverse = !demo->ease.reverse;
+        }
+        janus_UpdateEase(&demo->ease,elapsedTime);
+
+    }
+
     /* UI Demo */
     selected = janus_UpdateQuickUI(&demo->ui);
     
     if(selected) {
         demo->lastSelected = demo->ui.cursorPosition;
-        janus_Screenshake(4,4,2);
+        demo->scene = demo->ui.cursorPosition;
+        demo->playerX = PLAYER_STARTING_X;
+        demo->playerY = PLAYER_STARTING_Y;
     }
+
+    /* Animation Demo */
+    if(demo->scene == SCENE_MAIN_DEMO || demo->scene == SCENE_ANIMATION_DEMO) {
+        janus_UpdateAnimation(&demo->animation,elapsedTime);
+    }
+
 }
 void render(struct Demo* demo, float elapsedTime) {
     /* Clear Screen */
-    gfx_FillScreen(0xFF);
-    /* Debouncing/Animation Easing Demo */
-    gfx_SetColor(0x0F);
-    gfx_FillRectangle(demo->playerX,50,10,10);
-    /* Animation Easing Demo*/
-    gfx_SetColor(0xF0);
-    gfx_FillRectangle(demo->animation.currentX,demo->animation.currentY,10,10);
+    gfx_FillScreen(0x18);
+    /* Debouncing Demo */
+    if(demo->scene == SCENE_MAIN_DEMO || demo->scene == SCENE_DEBOUNCING_DEMO) {
+        gfx_SetColor(0x0F);
+        gfx_FillRectangle(demo->playerX,demo->playerY,10,10);
+        if(demo->scene == SCENE_DEBOUNCING_DEMO) {
+            gfx_SetTextFGColor(0x01);
+            gfx_PrintStringXY("USE ARROWS",5,LCD_HEIGHT-24);
+        }
+    }
+    /* Easing Demo */
+    if(demo->scene == SCENE_MAIN_DEMO || demo->scene == SCENE_EASING_DEMO) {
+        gfx_SetColor(0xF0);
+        gfx_FillRectangle(demo->ease.currentX,demo->ease.currentY,10,10);
+    }
     /* FPS Demo */
     renderFps(elapsedTime);
     /* UI Demo */
     janus_RenderQuickUI(&demo->ui);
-    gfx_PrintStringXY("Last Selected Option",5,220);
     gfx_PrintStringXY(demo->ui.options[demo->lastSelected],5,230);
+    /* Animation Demo */
+    if(demo->scene == SCENE_MAIN_DEMO || demo->scene == SCENE_ANIMATION_DEMO) {
+        char* frame = "12";
+        gfx_ScaledTransparentSprite_NoClip(janus_GetAnimationFrame(&demo->animation),LCD_WIDTH/2-wizard_tile_0->width*2,LCD_HEIGHT-(wizard_tile_1->height*4),4,4);
+        
+        sprintf(frame,"%d",demo->animation.currentFrame);
+        gfx_SetTextFGColor(0x1B);
+        gfx_PrintStringXY(frame,LCD_WIDTH/2-wizard_tile_0->width,LCD_HEIGHT-wizard_tile_1->height*4-10);
+    }
 }
 
 void renderFps(float elapsedTime) {
@@ -109,5 +173,6 @@ void renderFps(float elapsedTime) {
     if(elapsedTime < 1.0f/99)
         elapsedTime = 1.0f/99;
     sprintf(text,"FPS: %d",(uint8_t)(1/elapsedTime));
+    gfx_SetTextFGColor(0x01);
     gfx_PrintStringXY(text,270,10);
 }
